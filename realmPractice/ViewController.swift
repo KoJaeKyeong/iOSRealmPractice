8a7b5f89018c58c3338f0f1e8bee8e1c98fcf1bc
고재경 <folw159@gmail.com>
2021-03-30 09:48:21 +0900

    finish

--- realmPractice/ViewController.swift
+++ realmPractice/ViewController.swift
@@ -13,15 +13,69 @@ class ViewController: UIViewController {
     @IBOutlet weak var titleTextField: UITextField!
     @IBOutlet weak var directorTextField: UITextField!
     
+    var movie = Movie()
+    var realm: Realm!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    }
+        
+        realm = try! Realm()
 
+    }
+    
+    @IBAction func insertData(_ sender: Any) {
+        insertMovie()
+    }
+    
+    @IBAction func deleteData(_ sender: Any) {
+        deleteMovie()
+    }
+    
+    @IBAction func updateData(_ sender: Any) {
+        updateMovie()
+    }
+    
+    @IBAction func readData(_ sender: Any) {
+        readMovie()
+    }
 }
 
 extension ViewController {
     
-    var realm = Realm
+    func insertMovie() {
+        if let title = titleTextField.text {
+            movie.title = title
+        }
+        
+        if let director = directorTextField.text {
+            movie.director = director
+        }
+        
+        try! realm.write {
+            realm.add(movie)
+        }
+        
+        // Realm 경로확인
+        print(Realm.Configuration.defaultConfiguration.fileURL!)
+    }
+    
+    func deleteMovie() {
+        try! realm.write {
+            realm.delete(realm.objects(Movie.self))
+        }
+    }
+    
+    func updateMovie() {
+        if let movieInfo = realm.objects(Movie.self).filter(NSPredicate(format: "title = %@", titleTextField.text!)).first {
+            try! realm.write {
+                movieInfo.director = directorTextField.text!
+            }
+        }
+    }
+    
+    func readMovie() {
+        print(realm.objects(Movie.self))
+    }
     
 }
